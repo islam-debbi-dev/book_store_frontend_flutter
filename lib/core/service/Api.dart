896eb74bbb1c03e836fb011f14580c16cdf2af15
islam_debbi <islamdebbi6@gmail.com>
2025-05-03 17:51:05 +0200

    -  add get all books api

--- lib/core/service/Api.dart
+++ lib/core/service/Api.dart
@@ -1,3 +1,4 @@
+import 'package:booke_store/feattures/admin/adminhome/data/models/book.dart';
 import 'package:booke_store/feattures/login/data/models/login_req.dart';
 import 'package:booke_store/feattures/registration/data/models/registration_res.dart';
 import 'package:dio/dio.dart';
@@ -41,7 +42,6 @@ class Api {
 
       if (response.statusCode == 201) {
         final data = response.data as Map<String, dynamic>;
-        print(data);
         return RegistrationRes.fromJson(data);
       } else {
         final errorMessage = 'Failed to register: ${response.statusCode}';
@@ -53,91 +53,26 @@ class Api {
     }
   }
 
-  // Future<RegistrationRes> register(RegistrationReq registrationReq) async {
-  //   return [];
-  // }
+  Future<List<Book>> GetBooks() async {
+    try {
+      final response = await dio.get('$baseUrl$GetAllBooks');
 
-  // static Future<void> register(
-  //     String email, String username, String password,
-  //     BuildContext context) async {
-  //   try {
-  //     final response = await http.post(
-  //       Uri.parse('${baseUrl}auth/login'),
-  //       headers: <String, String>{
-  //         'Content-Type': 'application/json; charset=UTF-8',
-  //       },
-  //       body: jsonEncode(<String, String>{
-  //         'email': email,
-  //         'password': password,
-  //       }),
-  //     );
-
-  //     if (response.statusCode == 200) {
-  //       final data = jsonDecode(response.body);
-  //       // Save token or handle login success
-  //       ScaffoldMessenger.of(context).showSnackBar(
-  //         SnackBar(content: Text('Login successful')),
-  //       );
-  //       print('Login successful: ${data.toString()}');
-  //       Navigator.pushNamed(context, '/home');
-  //     } else {
-  //       final data = jsonDecode(response.body);
-  //       final errorMessage = data['message'] ?? 'Failed to login';
-  //       ScaffoldMessenger.of(context).showSnackBar(
-  //         SnackBar(content: Text(errorMessage)),
-  //       );
-  //       print('Failed to login: $errorMessage');
-  //       throw Exception(errorMessage);
-  //     }
-  //   } catch (e) {
-  //     final errorMessage = 'Failed to login: $e';
-  //     ScaffoldMessenger.of(context).showSnackBar(
-  //       SnackBar(content: Text(errorMessage)),
-  //     );
-  //     print(errorMessage);
-  //     throw Exception(errorMessage);
-  //   }
-  // }
-
-  // static Future<void> register(String email, String username, String password,
-  //     BuildContext context) async {
-  //   try {
-  //     final response = await http.post(
-  //       Uri.parse('${baseUrl}auth/register'),
-  //       headers: <String, String>{
-  //         'Content-Type': 'application/json; charset=UTF-8',
-  //       },
-  //       body: jsonEncode(<String, String>{
-  //         'email': email,
-  //         'username': username,
-  //         'password': password,
-  //       }),
-  //     );
-
-  //     if (response.statusCode == 201) {
-  //       final data = jsonDecode(response.body);
-  //       // Save token or handle registration success
-  //       ScaffoldMessenger.of(context).showSnackBar(
-  //         SnackBar(content: Text('Registration successful')),
-  //       );
-  //       print('Registration successful: ${data.toString()}');
-  //       Navigator.pushNamed(context, '/home');
-  //     } else {
-  //       final data = jsonDecode(response.body);
-  //       final errorMessage = data['message'] ?? 'Failed to register';
-  //       ScaffoldMessenger.of(context).showSnackBar(
-  //         SnackBar(content: Text(errorMessage)),
-  //       );
-  //       print('Failed to register: $errorMessage');
-  //       throw Exception(errorMessage);
-  //     }
-  //   } catch (e) {
-  //     final errorMessage = 'Failed to register: $e';
-  //     ScaffoldMessenger.of(context).showSnackBar(
-  //       SnackBar(content: Text(errorMessage)),
-  //     );
-  //     print(errorMessage);
-  //     throw Exception(errorMessage);
-  //   }
-  // }
+      if (response.statusCode == 200) {
+        // Assuming the response data is a list of book objects
+        final List<dynamic> data = response.data as List<dynamic>;
+        // Map each item in the list to a Book object
+        final List<Book> books = data
+            .map((bookJson) => Book.fromJson(bookJson as Map<String, dynamic>))
+            .toList();
+        return books;
+      } else {
+        final errorMessage = 'Failed to load books: ${response.statusCode}';
+        throw Exception(errorMessage);
+      }
+    } catch (e) {
+      // Handle Dio errors or other exceptions
+      final errorMessage = 'Failed to load books: $e';
+      throw Exception(errorMessage);
+    }
+  }
 }
